788f27ca66dd465c9a1c24f9af936ba57c87a1cf
KaloyanIT <kaloyanit@yahoo.com>
2017-03-29 22:59:42 +0300

    Add images

--- FZone/FZone/AppDelegate.swift
+++ FZone/FZone/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         return true
-    }
+    }    
     
     func loadLoginScreen() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
